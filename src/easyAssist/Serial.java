5c69cc22c16b33c5373f02ce7055d1ea285b8506
Fernando Partida Milanés <ferpart@hotmail.com>
2017-04-27 20:07:42 -0500

    Comments added

--- src/easyAssist/Serial.java
+++ src/easyAssist/Serial.java
@@ -352,7 +352,6 @@ public class Serial extends JPanel implements SerialPortEventListener{
 		
 	public static void main(String[] args) throws Exception {
 
-		
 		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		Serial main = new Serial();
 		main.initialize();
